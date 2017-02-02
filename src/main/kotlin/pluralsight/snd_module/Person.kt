60668ca04b5fd63f864fda53a2a2b784592f42f5
Iegor Maiorov <maiorov.prog@gmail.com>
2017-02-02 19:59:15 +0200

    inheritance example secondary constructors example

--- src/main/kotlin/pluralsight/snd_module/Person.kt
+++ src/main/kotlin/pluralsight/snd_module/Person.kt
@@ -4,11 +4,26 @@ interface Signatory {
     fun sign()
 }
 
-class Person : Signatory {
-    override fun sign() = println("I can sign a documents")
+open class Person(val name: String, var age: Int) : Signatory {
+
+    init {
+        if (name == "Kevin" && age > 125) throw Exception("Invalid age")
+    }
+
+    var isMarried: Boolean = false
+
+    constructor(name: String, age:Int, isMarried: Boolean) : this(name, age)
+    { this.isMarried = isMarried }
+
+    override fun sign() = println("$name who is $age years old can sign a documents (and their marital status is $isMarried)")
 }
 
+class Student(name: String, age: Int) : Person(name, age) {
+
+}
+
+
 fun main(args : Array<String>) {
-    val p = Person()
+    val p = Person("Kevin", 27, true)
     p.sign()
 }
