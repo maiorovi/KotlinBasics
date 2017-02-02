bcd8682a63c9b8a78a63a36b7948afa34732eeaf
Iegor Maiorov <maiorov.prog@gmail.com>
2017-02-02 20:13:52 +0200

    jvmstatic explored

---
+++ src/main/kotlin/pluralsight/snd_module/Entity.kt
@@ -0,0 +1,18 @@
+package pluralsight.snd_module
+
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
+class Entity(val name:String, val traitOne:String, val date: LocalDate) {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val ent = Entity("name", "trait1", LocalDate.of(2017,10,10));
+            println(ent)
+        }
+    }
+
+}
+
