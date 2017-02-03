809f81344090441c06d70be4637ca676cc3e2a02
Iegor Maiorov <maiorov.prog@gmail.com>
2017-02-03 23:46:17 +0200

    getting started with kluent lib
    spoiled few fucking hours working with jfrog((

--- src/test/kotlin/pluralsight/testing/CalculatorSpec.kt
+++ src/test/kotlin/pluralsight/testing/CalculatorSpec.kt
@@ -1,9 +1,14 @@
 package pluralsight.testing
 
+import org.amshove.kluent.shouldBe
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.assertj.core.api.Assertions.*
 
+@RunWith(JUnitPlatform::class)
 class CalculatorSpec : Spek({
     val calculator = Calculator(NullResult())
 
@@ -11,7 +16,7 @@ class CalculatorSpec : Spek({
 
         it("should add two numbers") {
             val result = calculator.add(8, 10)
-
+            result shouldBe 18
         }
 
     }
