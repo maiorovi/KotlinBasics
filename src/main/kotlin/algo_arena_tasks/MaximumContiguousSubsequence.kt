26256612125971944e9534d2f031bc599305f44b
Iegor Maiorov <maiorov.prog@gmail.com>
2017-02-06 23:55:28 +0200

    hopefully correct solution of problem using kardans algorithm

--- src/main/kotlin/algo_arena_tasks/MaximumContiguousSubsequence.kt
+++ src/main/kotlin/algo_arena_tasks/MaximumContiguousSubsequence.kt
@@ -1,19 +1,32 @@
 package algo_arena_tasks
 
-fun maxContSequence(arr: IntArray): Int {
+fun maxContSequence(arr: IntArray): Array<Int> {
     var maxEndingHere = arr[0]
     var maxSoFar = arr[0]
-
+    var startIndex = 0
+    var endIndex = 0
     var i = 1
 
     while( i < arr.size) {
+        if (maxEndingHere + arr[i] > arr[i]) {
+            maxEndingHere = maxEndingHere + arr[i]
+        } else {
+            maxEndingHere = arr[i]
 
-        maxEndingHere = Math.max(maxEndingHere + arr[i], arr[i])
-        maxSoFar = Math.max(maxEndingHere, maxSoFar)
+            if (maxEndingHere > maxSoFar) {
+                startIndex = i
+            }
+        }
+
+        if (maxEndingHere > maxSoFar) {
+            maxSoFar = maxEndingHere
+            endIndex = i
+        }
+
 
         i++
     }
 
 
-    return maxSoFar
+    return arrayOf(maxSoFar, startIndex, endIndex)
 }
--- src/test/kotlin/algo_arena_tasks/MaximumContiguousSubsequenceSpec.kt
+++ src/test/kotlin/algo_arena_tasks/MaximumContiguousSubsequenceSpec.kt
@@ -1,28 +1,32 @@
 package algo_arena_tasks
 
 import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeIn
+import org.amshove.kluent.shouldEqual
+import org.amshove.kluent.shouldContain
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
+import stuff.array
 
 @RunWith(JUnitPlatform::class)
 class MaximumContiguousSubsequenceSpec : Spek({
     describe("maximum contiguous subsequence spec") {
         it("find maximum subsequence in simple sequence") {
-            maxContSequence(intArrayOf(-1,-2,3,4,5)) shouldBe 12
+            maxContSequence(intArrayOf(-1,-2,3,4,5)) shouldEqual  arrayOf(12, 2, 4)
 
-            maxContSequence(intArrayOf(1,2,3,-10, 5)) shouldBe 6
-            maxContSequence(intArrayOf(-1,-2,-3,-4,-5)) shouldBe -1
+            maxContSequence(intArrayOf(1,2,3,-10, 5)) shouldEqual  arrayOf(6, 0, 2)
+            maxContSequence(intArrayOf(-1,-2,-3,-4,-5)) shouldEqual  arrayOf(-1, 0, 0)
         }
 
         it("test case 2") {
-            maxContSequence(intArrayOf(1,2,3,-2,5)) shouldBe 9
+            maxContSequence(intArrayOf(1,2,3,-2,5)) shouldEqual arrayOf(9, 0, 4)
         }
 
         it("test case 3") {
-            maxContSequence(intArrayOf(-2, -3, 4, -1, -2, 1, 5, -3)) shouldBe 7
+            maxContSequence(intArrayOf(-2, -3, 4, -1, -2, 1, 5, -3)) shouldEqual arrayOf(7, 2, 6)
         }
     }
 })
