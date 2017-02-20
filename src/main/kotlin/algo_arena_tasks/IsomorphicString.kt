498de5f6a295a3482a66a21866d8a0a5d247a826
egor <maiorov.prog@gmail.com>
2017-02-20 21:13:17 +0200

    count path task solved

---
+++ src/main/kotlin/algo_arena_tasks/IsomorphicString.kt
@@ -0,0 +1,25 @@
+package algo_arena_tasks
+
+fun isIsomorphic(input1: String, input2:String): Boolean {
+    val mappings = hashMapOf<Char, Char>()
+
+    if (input1.length != input2.length) {
+        return false
+    }
+
+    for (i in 0..input1.length - 1) {
+        val ch = input1[i]
+        if (!mappings.containsKey(ch)) {
+            mappings.put(ch, input2[i])
+        } else {
+            if (mappings[ch] != input2[i]) {
+                return false
+            }
+        }
+    }
+
+
+    return true
+}
+
+
