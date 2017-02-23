1c6f9f70a3523a564b5b249f78078bc4f93411ee
egor <maiorov.prog@gmail.com>
2017-02-23 18:58:22 +0200

    zero matrix problem solved

---
+++ src/main/kotlin/algo_arena_tasks/FindMaxSumLevel.kt
@@ -0,0 +1,18 @@
+package algo_arena_tasks
+
+import data_structures.TreeNode
+
+
+fun findMaxSumLevel(root: TreeNode?):Int {
+    return 0
+}
+
+/*
+
+
+      1
+     / \
+    2   3 ==> 2
+   / \  /\
+   4 5 6  7
+ */
